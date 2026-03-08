555f6b403f79dc3ad2a693f78879d72790819069
Sabrina Demetrio <sdemetrio@globalnewsgroup.com>
2026-03-08 19:55:36 -0300

    configuración de eslint/prettier + mockUp de prueba

--- components/ui/collapsible.tsx
+++ components/ui/collapsible.tsx
@@ -1,15 +1,33 @@
-import { PropsWithChildren, useState } from 'react';
-import { StyleSheet, TouchableOpacity } from 'react-native';
-
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import * as Notifications from 'expo-notifications';
+import { PropsWithChildren, useState } from 'react';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldPlaySound: true,
+        shouldSetBadge: true,
+        shouldShowBanner: true,
+        shouldShowList: true,
+    }),
+});
 
 export function Collapsible({ children, title }: PropsWithChildren & { title: string }) {
   const [isOpen, setIsOpen] = useState(false);
   const theme = useColorScheme() ?? 'light';
+
+  console.log('sección Explore?')
+
+  async function triggerPushNotifications() {
+    //Configurar canal (iOS)
+    const { status } = await Notifications.requestPermissionsAsync();
+    if (status !== "granted") return;
+
+  }
 
   return (
     <ThemedView>
--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,25 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+// configurar según nuevo eslint 9
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*'],
+    ignores: ["dist/*"],
+    plugins: ["react", "@typescript-eslint", "prettier"],
+    rules: {
+      "no-var": 2,
+      eqeqeq: 2,
+      "no-console": 0,
+      "require-await": 1,
+      "no-unused-vars": 1,
+      "no-inline-comments": 0,
+      "no-duplicate-imports": 1,
+      "array-callback-return": 0,
+      "react/no-children-prop": 0,
+      "react-hooks/exhaustive-deps": 0,
+      "react/prop-types": [0, { ignore: ["children"] }],
+    },
   },
 ]);
