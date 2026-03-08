0c532ce54b57a17a014824ca0477ab2acdc31cb1
SabriBere <95036708+SabriBere@users.noreply.github.com>
2026-03-08 19:55:36 -0300

    configuración de eslint/prettier + mockUp de prueba

---
+++ mocks/mockUpsAlert.ts
@@ -0,0 +1,11 @@
+export const notificationList = [
+    {
+        Title: "Kia transportó Trofeo FIFA World Cup 2026 en el Trophy Tour Quito",
+        Tier: 1,
+        Tone: 1,
+        MediaType: 4,
+        Media: "Automagazine (Online)",
+        Section: "Deportes",
+        Link: "https://globalne.ws/v2aETwbSAy5pkI0wLhM39mAV8UawofctvsXHKJ3V-pE",
+    },
+];
