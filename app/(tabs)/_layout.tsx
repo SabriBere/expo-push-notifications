e766f681aa3f8e699a7bd1787d2a8429e96e65dc
SabriBere <95036708+SabriBere@users.noreply.github.com>
2026-03-09 01:03:53 -0300

    inicio de maquetado Hist. Notificaciones

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -19,17 +19,18 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Notifications',
+          title: 'Settings',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="notifications"
+        options={{
+          title: 'Notifications',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+        }}
+      />
+      
     </Tabs>
   );
 }
