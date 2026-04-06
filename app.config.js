4d08021671579e56b24a1b602cab6af0dce738ba
Sabrina Demetrio <sdemetrio@globalnewsgroup.com>
2026-04-06 16:33:50 -0300

    config credentials + config eas build

--- app.config.js
+++ app.config.js
@@ -1,24 +1,69 @@
-const appJson = require("./app.json");
-
 module.exports = () => {
-  const config = appJson.expo;
   const projectId =
-    process.env.EXPO_PUBLIC_EAS_PROJECT_ID ?? config.extra?.eas?.projectId;
-  const googleServicesFile =
-    process.env.GOOGLE_SERVICES_JSON ?? config.android?.googleServicesFile;
+    process.env.EXPO_PUBLIC_EAS_PROJECT_ID ??
+    "4eabd014-afb6-4f98-a786-686be27b4bfb";
+  const googleServicesFile = process.env.GOOGLE_SERVICES_JSON;
 
   return {
-    ...config,
-    extra: {
-      ...config.extra,
-      eas: {
-        ...config.extra?.eas,
-        ...(projectId ? { projectId } : {}),
-      },
+    name: "TestNotifications",
+    slug: "testnotifications",
+    owner: "sabrinademetrios-organization",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "testnotifications",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
     },
     android: {
-      ...config.android,
+      package: "com.sdemetrio.testnotifications",
+      adaptiveIcon: {
+        backgroundColor: "#E6F4FE",
+        foregroundImage: "./assets/images/android-icon-foreground.png",
+        backgroundImage: "./assets/images/android-icon-background.png",
+        monochromeImage: "./assets/images/android-icon-monochrome.png",
+      },
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
       ...(googleServicesFile ? { googleServicesFile } : {}),
+    },
+    web: {
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-notifications",
+        {
+          icon: "./assets/images/icon.png",
+          color: "#ffffff",
+          defaultChannel: "default",
+        },
+      ],
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+          dark: {
+            backgroundColor: "#000000",
+          },
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true,
+    },
+    extra: {
+      eas: {
+        ...(projectId ? { projectId } : {}),
+      },
     },
   };
 };
